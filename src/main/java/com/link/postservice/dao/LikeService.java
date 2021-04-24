972711df26ac55c59b05103b16430a4cd2e083d6
Luis Miranda <41742635+luismir15@users.noreply.github.com>
2021-04-24 14:56:28 -0400

    tested delete like

--- src/main/java/com/link/postservice/dao/LikeService.java
+++ src/main/java/com/link/postservice/dao/LikeService.java
@@ -9,9 +9,9 @@ import java.util.List;
 public interface LikeService extends JpaRepository<Like, Integer> {
     //    public void insert(Like like);
 //    public void update(Like like);
-//    public void delete(Like like);
+    public void delete(Like like);
 //    public Like findById(int LikeId);
-//    public List<Like> findAll();
+    public List<Like> findAll();
     public List<Like> findAllByPostPostId(int postId);
     public List<Like> findAllByUserId(int userId);
 }
