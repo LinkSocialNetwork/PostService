b8e6d080bcdcd27e41b296f179a09e87657bc212
m-loutfi <michaelldaa@gmail.com>
2021-04-26 16:49:57 -0400

    updates testdata.sql

--- src/test/resources/testdata.sql
+++ src/test/resources/testdata.sql
@@ -1,10 +1,10 @@
-<<<<<<< HEAD
+
 INSERT INTO posts (post_string) VALUES ('my first post');
-=======
+
 INSERT INTO posts_users (profile_img_url, user_name) VALUES ('/asdfdsaasd', 'one');
 INSERT INTO posts_users (profile_img_url, user_name) VALUES ('/adasfdsfad', 'two');
 INSERT INTO posts_users (profile_img_url, user_name) VALUES ('/adsdsaf', 'three');
 INSERT INTO posts_users (profile_img_url, user_name) VALUES ('/asdfwqweasd', 'four');
 INSERT INTO posts_users (profile_img_url, user_name) VALUES ('/qwewqeqwe', 'five');
 INSERT INTO posts_users (profile_img_url, user_name) VALUES ('/bcnbncvbc', 'six');
->>>>>>> ae531fb92e12b07d87d5fdc6729d0e247e63d36f
+
