8df4f4ec4fadc23453a8391b1fca7f7735977efa
So0123 <so.morishima@gmail.com>
2021-04-26 11:08:04 -0500

    updatePost test completed

--- src/test/java/com/link/postservice/controller/PostControllerTest.java
+++ src/test/java/com/link/postservice/controller/PostControllerTest.java
@@ -1,15 +1,34 @@
 package com.link.postservice.controller;
 
+import com.link.postservice.model.Post;
+import com.link.postservice.service.LikeService;
+import com.link.postservice.service.PostService;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
+@ExtendWith(MockitoExtension.class)
 class PostControllerTest {
+
+    @Mock
+    private PostService postService;
+
+    @Mock
+    private LikeService likeService;
+
+    private PostController postController;
 
     @BeforeEach
     void setUp() {
+        postController = new PostController(postService, likeService);
     }
 
     @AfterEach
@@ -18,5 +37,11 @@ class PostControllerTest {
 
     @Test
     void updatePost() {
+        Post post = new Post();
+
+        postController.updatePost(post);
+
+        Mockito.verify(postService).updatePost(post);
+
     }
 }
