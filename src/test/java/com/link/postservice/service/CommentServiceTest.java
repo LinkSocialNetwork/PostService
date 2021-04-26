0f7b428daf9a0fb6ba3f093110723623204d8bf5
So0123 <so.morishima@gmail.com>
2021-04-26 10:35:09 -0500

    addComment test completed

--- src/test/java/com/link/postservice/service/CommentServiceTest.java
+++ src/test/java/com/link/postservice/service/CommentServiceTest.java
@@ -1,15 +1,31 @@
 package com.link.postservice.service;
 
+import com.link.postservice.dao.CommentDao;
+import com.link.postservice.model.Comment;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
+@ExtendWith(MockitoExtension.class)
 class CommentServiceTest {
+
+    @Mock
+    CommentDao commentDao;
+
+    CommentService commentService;
 
     @BeforeEach
     void setUp() {
+        commentService = new CommentService(commentDao);
     }
 
     @AfterEach
@@ -18,5 +34,11 @@ class CommentServiceTest {
 
     @Test
     void addComment() {
+        Comment comment = new Comment();
+        Mockito.when(this.commentDao.save(comment)).thenReturn(comment);
+        commentService.addComment(comment);
+
+        ArgumentCaptor<Comment> myCaptor = ArgumentCaptor.forClass(Comment.class);
+        Mockito.verify(commentDao).save(myCaptor.capture());
     }
 }
