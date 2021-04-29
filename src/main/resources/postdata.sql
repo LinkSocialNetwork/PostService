ea290126b532eeddebaa4f493ce30c16a9349bf8
chakeem95 <chakeem95@gmail.com>
2021-04-29 12:02:17 -0700

    postservice h2database

---
+++ src/main/resources/postdata.sql
@@ -0,0 +1,13 @@
+INSERT INTO posts_users (user_id, user_name) VALUES (1, 'admin')
+INSERT INTO posts_users (user_id, user_name) VALUES (2, 'admin2')
+
+INSERT INTO posts (post_content, user_id) VALUES ('post 1', 1)
+INSERT INTO posts (post_content, user_id) VALUES ('post 2', 1)
+INSERT INTO posts (post_content, user_id) VALUES ('post 3', 1)
+INSERT INTO posts (post_content, user_id) VALUES ('post 4', 1)
+
+INSERT INTO posts (post_content, user_id) VALUES ('post 1', 2)
+INSERT INTO posts (post_content, user_id) VALUES ('post 2', 2)
+
+
+
