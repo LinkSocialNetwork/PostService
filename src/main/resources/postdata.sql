7e1e0972ec825b05eee643d2b320e43ebdaa3888
Kevin Desktop <kchilds2020@gmail.com>
2021-05-03 09:00:17 -0500

    added users to postservice model to match userservice model

--- src/main/resources/postdata.sql
+++ src/main/resources/postdata.sql
@@ -1,5 +1,13 @@
 INSERT INTO posts_users (user_id, user_name) VALUES (1, 'admin')
 INSERT INTO posts_users (user_id, user_name) VALUES (2, 'admin2')
+INSERT INTO posts_users (user_id, user_name) VALUES (3, 'admin3')
+INSERT INTO posts_users (user_id, user_name) VALUES (4, 'admin4')
+INSERT INTO posts_users (user_id, user_name) VALUES (5, 'admin5')
+INSERT INTO posts_users (user_id, user_name) VALUES (6, 'admin6')
+INSERT INTO posts_users (user_id, user_name) VALUES (7, 'admin7')
+INSERT INTO posts_users (user_id, user_name) VALUES (8, 'admin8')
+INSERT INTO posts_users (user_id, user_name) VALUES (9, 'admin9')
+INSERT INTO posts_users (user_id, user_name) VALUES (10, 'admin10')
 
 INSERT INTO posts (post_content, user_id) VALUES ('post 1', 1)
 INSERT INTO posts (post_content, user_id) VALUES ('post 2', 1)
